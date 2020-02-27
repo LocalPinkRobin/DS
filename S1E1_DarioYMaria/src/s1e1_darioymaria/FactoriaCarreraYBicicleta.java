2f638f524b3c9a7eca025328295b59949933eeb7
Darío Megías <d.megias.guerrero@gmial.com>
2020-02-27 19:56:42 +0100

    Hasiendo cosas raras

--- S1E1_DarioYMaria/src/s1e1_darioymaria/FactoriaCarreraYBicicleta.java
+++
@@ -1,7 +0,0 @@
-package s1e1_darioymaria;
-
-public interface FactoriaCarreraYBicicleta {
-    public abstract Carrera crearCarrera(int n);
-    public abstract Bicicleta crearBicicleta(int id);
-    
-}
