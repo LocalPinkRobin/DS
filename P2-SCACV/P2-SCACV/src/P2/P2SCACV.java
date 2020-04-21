21e405ef452d5d02bda6a118016a71e58c1327ff
Darío Megías <d.megias.guerrero@gmial.com>
2020-04-21 10:11:03 +0200

    Problemas

---
+++ P2-SCACV/P2-SCACV/src/P2/P2SCACV.java
@@ -0,0 +1,17 @@
+/*
+ * encoding:utf-8
+ * 
+ * Autores: María Sánchez Marcos y Darío Megías Guerrero
+ */
+package P2;
+
+public class P2SCACV {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
